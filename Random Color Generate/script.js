b8d988185139b14e6fcc91187b3102705101d46f
Ankesh kumar <ankeshkumarrajak39@gmail.com>
2026-06-26 20:42:03 +0530

    color change

---
+++ Random Color Generate/script.js
@@ -0,0 +1,11 @@
+let changeBtn=document.querySelector("#btn");
+let body=document.querySelector("body");
+changeBtn.addEventListener("click",()=>{
+    const hex="0123456789ABCDEF";
+let color ="#";
+for(let i=0;i<6;i++){
+    color+= hex[Math.floor(Math.random()*16)];
+
+}
+  body.style.backgroundColor = color;
+})
